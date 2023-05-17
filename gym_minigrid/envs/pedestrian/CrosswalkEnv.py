6439a26db395a2a87171d5c39fbc4d482c40fc6d
zarifikram <65703522+zarifikram@users.noreply.github.com>
2023-05-17 19:44:05 +0600

    Update CrosswalkEnv.py

--- gym_minigrid/envs/pedestrian/CrosswalkEnv.py
+++ gym_minigrid/envs/pedestrian/CrosswalkEnv.py
@@ -1,17 +1,15 @@
+from gym_minigrid.agents.Agent import Agent
 from .PedestrianEnv import MultiPedestrianEnv
-
+from typing import List
 
 class CrosswalkEnv(MultiPedestrianEnv):
-
-
-    
     def __init__(
         self,
         agents: List[Agent]=None,
         width=8,
         height=8,
-        crosswalkPosition=None
-        crosswalkOrigin=None
+        crosswalkPosition=None,
+        crosswalkOrigin=None,
         crosswalkSize = 3
     ):
 
@@ -23,9 +21,9 @@ class CrosswalkEnv(MultiPedestrianEnv):
         if crosswalkOrigin is None:
             crosswalkOrigin = (width // 2, crosswalkPosition[1] + (crosswalkSize // 2))
 
-        this.crosswalkOrigin = crosswalkOrigin
-        this.crosswalkPosition = crosswalkPosition
-        this.crosswalkSize = crosswalkSize
+        self.crosswalkOrigin = crosswalkOrigin
+        self.crosswalkPosition = crosswalkPosition
+        self.crosswalkSize = crosswalkSize
 
     
     def getCrossWalkPoint(self, worldPoint):
