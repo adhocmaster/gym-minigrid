b594b06e0503692958f64ca461e37ee69d75a872
AmeliaY07 <ameliayuan07@gmail.com>
2023-08-16 09:36:53 -0700

    changed trajectory vehicle test for the GIF

--- test-trajectory-vehicle.py
+++ test-trajectory-vehicle.py
@@ -1,4 +1,4 @@
-exec(open("sys_path_hack.py").read())
+#exec(open("sys_path_hack.py").read())
 
 import gym
 import pedgrid
@@ -16,10 +16,10 @@ vehicle1 = TrajectoryVehicle(
     id=1,
     topLeft=(10, 10),
     bottomRight=(15, 20),
-    trajectory=(12, 12, 14, 14, 16, 16, 18, 18, 20, 20),
+    trajectory=(10, 10, 12, 12, 14, 14, 16, 16, 18, 18, 17, 20, 15, 22, 12, 24, 11, 25, 13, 27),
     direction=2,
-    maxSpeed=1.0,
-    speed=0.5,
+    maxSpeed=1.5,
+    speed=1.2,
     inRoad=1,
     inLane=1,
 )
@@ -28,8 +28,8 @@ env.addVehicleAgent(vehicle1)
 
 
 #Move the vehicle along the trajectory
-for i in range(5):
+for i in range(9):
     env.step(None)
     env.render()
-    time.sleep(2)
+    time.sleep(0.2)
 env.close()
