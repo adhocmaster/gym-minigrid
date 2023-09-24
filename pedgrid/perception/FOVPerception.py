1dc0adf8126cda5657ce0d3bd86fc4f7f6596032
camcimahir <camcimahir@gmail.com>
2023-09-24 12:02:52 -0700

    FOVPerception

--- pedgrid/perception/FOVPerception.py
+++ pedgrid/perception/FOVPerception.py
@@ -33,133 +33,41 @@ from shapely.geometry import Polygon, Point
 class FOVPerception(BasePerception):
 
 
-
     def getOG(self, agentLocation: Tuple[int, int], gridSize: Tuple[int, int], objects: List[BaseObject]) -> List[List[float]]:
 
         obsticlePolygons = self._objsToMultiPolygon(objects)
 
-        occupation_grid= []
-
         grid = self._createGrid(gridSize)
 
-        for i in range(len(grid)):
-            occupation_grid.append(0.0)
+        occupation_grid = self._fillOccupationGridtoZero(grid)
 
         #this is the main part of the function here we iterate through each object draw appropriate lines and create a polygon representing the blind spot
         for i in range(len(obsticlePolygons.geoms)):
 
             exterior_ring1 = obsticlePolygons.geoms[i]
             print(exterior_ring1)
-            vertices_coords1 = list(exterior_ring1.coords)  
-            print(vertices_coords1)
+            vertices_coords = list(exterior_ring1.exterior.coords)  
 
-            def remove_closest_furthest(vertices_coords1: List[Tuple[float, float]], agent_location: Tuple[float, float]) -> Tuple[Tuple[float, float], Tuple[float, float]]:
+            vertice_max = self._get_closest_furthest(vertices_coords, agentLocation)[0]
 
-                def distance(point1: Tuple[float, float], point2: Tuple[float, float]) -> float:
-                    x1, y1 = point1
-                    x2, y2 = point2
-                    return np.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+            vertices_coords = self._remove_closest_furthest(vertices_coords, agentLocation) #this is the vertices_coords with the furthest and closest points removed and I will not need the older version
 
-                # Get the closest and furthest points from the pedestrian
-                distances = [distance(vertex, agent_location) for vertex in vertices_coords1] # stores each distance from vertice coords
-                closest_point_index = np.argmin(distances)
-                furthest_point_index = np.argmax(distances)
-
-                # Remove the closest and furthest points from vertices_coords1 save the furthest to be used later
-                vertices_coords1.pop(closest_point_index)
-                furthest_point = vertices_coords1.pop(furthest_point_index - 1 if furthest_point_index > closest_point_index else furthest_point_index)
-
-                return furthest_point
-
-            vertice_max = remove_closest_furthest(vertices_coords1, agentLocation)
-
-            def extend_line(agentLocation, vertices_coords, vertex_index, extension_length=100):
-
-                vertex_coords = vertices_coords[vertex_index]
-                dx = vertex_coords[0] - agentLocation[0]
-                dy = vertex_coords[1] - agentLocation[1]
-                magnitude = (dx ** 2 + dy ** 2) ** 0.5
-                unit_vector_x = dx / magnitude
-                unit_vector_y = dy / magnitude
-                new_endpoint_x = vertex_coords[0] + unit_vector_x * extension_length
-                new_endpoint_y = vertex_coords[1] + unit_vector_y * extension_length
-
-                extended_line = LineString([(agentLocation[0], agentLocation[1]), (new_endpoint_x, new_endpoint_y)])
-                return extended_line
-
-            extended_line0 = extend_line(agentLocation, vertices_coords1, 0, len(grid))
-            extended_line1 = extend_line(agentLocation, vertices_coords1, 1, len(grid))
+             
+            LineagentToVertexAndBeyondGrid0 = self._extend_line(agentLocation, vertices_coords, 0, gridSize)
+            LineagentToVertexAndBeyondGrid1 = self._extend_line(agentLocation, vertices_coords, 1, gridSize)
 
 
-            s2, t2 = extended_line0.xy
-            plt.plot(s2, t2, color="green")
-            s3, t3 = extended_line1.xy
-            plt.plot(s3, t3, color="green")
+            self._plotLine(self._getLineCoords(LineagentToVertexAndBeyondGrid0)[0], self._getLineCoords(LineagentToVertexAndBeyondGrid0)[1])
+            self._plotLine(self._getLineCoords(LineagentToVertexAndBeyondGrid1)[0], self._getLineCoords(LineagentToVertexAndBeyondGrid1)[1])
 
-
-            def NotVisiblePolygon():
-                grid_boundary = box(0, 0, 100, 100)
-                x, y = grid_boundary.exterior.xy
-                plt.plot(x, y, color="blue", alpha=0.5)
-                intersection1 = grid_boundary.intersection(extended_line0)
-                intersection2 = grid_boundary.intersection(extended_line1)
-
-                intersection11 = intersection1.coords[1]
-                intersection22 = intersection2.coords[1]
-
-                # Create the polygon representing the field of view behind the obsticlePolygons
-                rear_polygon_coords = [vertices_coords1[0]]
-
-                #rear_polygon_coords.append(vertices_coords1[0])
-                rear_polygon_coords.append(vertice_max)
-                rear_polygon_coords.append(vertices_coords1[1])
-                rear_polygon_coords.append(intersection22)
-                if ((intersection1.coords[1][0] == 0.0 and intersection1.coords[1][1] != 100.0 and intersection2.coords[1][0] != 0.0 and intersection2.coords[1][1] == 100.0) or (intersection1.coords[1][0] != 0.0 and intersection2.coords[1][0] == 0.0 and intersection2.coords[1][1] != 100.0 and intersection1.coords[1][1] == 100.0)):
-                    rear_polygon_coords.append((0.0, 100.0))
-                elif ((intersection1.coords[1][0] == 100.0 and intersection2.coords[1][0] != 100.0 and intersection2.coords[1][1] == 100.0) or (intersection1.coords[1][0] != 100.0 and intersection2.coords[1][0] == 100.0 and intersection1.coords[1][1] == 100.0)):
-                    rear_polygon_coords.append((100.0, 100.0))
-                elif ((intersection1.coords[1][0] == 0.0 and intersection2.coords[1][0] != 0.0 and intersection2.coords[1][0] != 0.0) or (intersection1.coords[1][0] != 0.0 and intersection2.coords[1][0] == 0.0 and intersection2.coords[1][1] != 0.0)):
-                    rear_polygon_coords.append((0.0, 0.0))
-                elif ((intersection1.coords[1][0] == 100.0 and intersection2.coords[1][0] != 100.0 and intersection2.coords[1][1] == 0.0) or (intersection1.coords[1][0] != 100.0 and intersection2.coords[1][0] == 100.0 and intersection1.coords[1][1] == 0.0)):
-                    rear_polygon_coords.append((100.0, 0.0))
-                rear_polygon_coords.append(intersection11)
-
-
-                rear_polygon = Polygon(rear_polygon_coords)
-
-                x, y = rear_polygon.exterior.xy
-                plt.plot(x, y, color="black", alpha=0.5)
-                plt.fill(x ,y, alpha=1, color="blue")
-
-                return rear_polygon
-
-            def fillNotVisiblePolygon():
-                for i in range(len(grid)):
-                    if ((grid[i]).intersects(NotVisiblePolygon())):
-                        occupation_grid[i] = (0.5)
-            fillNotVisiblePolygon()
-
-        def fillOccupancyGrid():
-            for i in range(len(grid)):
-                if ((occupation_grid[i]) == 0.5):
-                    occupation_grid[i] = (0.5)
-                elif (grid[i]).intersects(obsticlePolygons):
-                    occupation_grid[i] = (1.0)
-                else:
-                    occupation_grid[i] = (0.0)
+    
+            self._fillNotVisiblePolygon(grid, occupation_grid, vertices_coords, LineagentToVertexAndBeyondGrid0, LineagentToVertexAndBeyondGrid1, vertice_max, gridSize)
  
-        fillOccupancyGrid()
+        self._fillOccupancyGrid(grid, occupation_grid, obsticlePolygons)
 
-        # Convert the list to a 10x10 2D list
-        cols = int(math.sqrt(len(grid)))
-        occupation_grid = [occupation_grid[i:i + cols] for i in range(0, len(occupation_grid), cols)]
-
-        # Print the result
-        for row in occupation_grid:
-            print(row)
-
-        plt.title("line")
-        plt.show()
+        occupation_grid = self._1dTo2dGrid(occupation_grid, gridSize[0], gridSize[1])
+        
+        self._printOccupationGrid(occupation_grid)
 
         return occupation_grid
     
@@ -169,21 +77,30 @@ class FOVPerception(BasePerception):
         grid = []
         for i in range(gridSize[0]):
             for j in range(gridSize[1]):
-                cell = box(10*i, 10*j, 10*i+10, 10*j+10)
+                cell = box((gridSize[0]/10)*i, (gridSize[1]/10)*j, (gridSize[0]/10)*i+1, (gridSize[1]/10)*j+1)
                 grid.append(cell)
         return grid
+
     
-    def _plotPoly(poly: Polygon, color="black", linewidth=0.5):
+    def _plotPoly(self, poly: Polygon, color="black", linewidth=0.5):
         x1, y1 = poly.exterior.xy
         plt.plot(x1, y1, color=color, linewidth=linewidth)
+        plt.show()
 
+    def _fillOccupationGridtoZero(self, grid: List) -> List:
+        Zero = []
+        for i in range(len(grid)):
+            Zero.append(0.0)
+        return Zero
 
-    def _objToPolygon(obj: BaseObject) -> Polygon:
-
-        polygon = Polygon([(obj.topLeft[0], obj.topLeft[1]),
-                            (obj.bottomRight[0], obj.topLeft[1]),
-                            (obj.bottomRight[0], obj.bottomRight[1]),
-                            (obj.topLeft[0], obj.bottomRight[1])])
+    def _objToPolygon(self, obj: BaseObject) -> Polygon:
+        polygon = Polygon([
+                (obj.topLeft[0], obj.topLeft[1]),
+                (obj.bottomRight[0], obj.topLeft[1]),
+                (obj.bottomRight[0], obj.bottomRight[1]),
+                (obj.topLeft[0], obj.bottomRight[1]),
+                (obj.topLeft[0], obj.topLeft[1])  # Close the ring by repeating the first point.
+                        ])
         assert polygon.is_valid
         return polygon
     
@@ -191,6 +108,145 @@ class FOVPerception(BasePerception):
         polygons = []
         for obj in objs:
             polygons.append(self._objToPolygon(obj))
+        
         return MultiPolygon(polygons)
+    
+    def _distance(self, point1: Tuple[float, float], point2: Tuple[float, float]) -> float:
+        x1, y1 = point1
+        x2, y2 = point2
+        return np.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+    
+    def _get_closest_furthest(self, vertices_coords: List[Tuple[float, float]], agent_location: Tuple[float, float]) -> Tuple[Tuple[float, float], Tuple[float, float]]:
 
+        # Get the closest and furthest points from the pedestrian
+        distances = [self._distance(vertex, agent_location) for vertex in vertices_coords] # stores each distance from vertice coords
+        closest_point_index = np.argmin(distances)
+        furthest_point_index = np.argmax(distances)
+
+        return vertices_coords[furthest_point_index], vertices_coords[closest_point_index]
+
+    
+    def _remove_closest_furthest(self, vertices_coords: List[Tuple[float, float]], agent_location: Tuple[float, float] ) -> Tuple[Tuple[float, float], Tuple[float, float]]:
+
+        vertices_coords_temp = vertices_coords
+        furthest_point, closest_point = self._get_closest_furthest(vertices_coords, agent_location)
+
+        # Remove the closest and furthest points from vertices_coords save the furthest to be used later
+        closest_point = vertices_coords_temp.remove(closest_point)
+        furthest_point = vertices_coords_temp.remove(furthest_point)
+
+        return vertices_coords_temp
+    
+    def _extend_line(self, agentLocation: Tuple[int, int], vertices_coords: List[Tuple[float, float]], vertex_index: int, gridSize: Tuple[int, int]) -> LineString:
+
+        extension_length = (1.5*gridSize[0])
+        vertex_coords = vertices_coords[vertex_index]
+        dx = vertex_coords[0] - agentLocation[0]
+        dy = vertex_coords[1] - agentLocation[1]
+        magnitude = (dx ** 2 + dy ** 2) ** 0.5
+        unit_vector_x = dx / magnitude
+        unit_vector_y = dy / magnitude
+        new_endpoint_x = vertex_coords[0] + unit_vector_x * extension_length
+        new_endpoint_y = vertex_coords[1] + unit_vector_y * extension_length
+
+        extended_line = LineString([(agentLocation[0], agentLocation[1]), (new_endpoint_x, new_endpoint_y)])
+
+        return extended_line
+    
+    def _getLineCoords(self, line: LineString) -> Tuple[List[float], List[float]]:
+        x, y = line.xy
+        return x, y
+    
+    def _plotLine(self, x: List[float], y: List[float]):
+        plt.plot(x, y, color="green")
+
+    def _fillNotVisiblePolygon(self, grid, occupation_grid, vertices_coords: List[Tuple[float, float]], LineagentToVertexAndBeyondGrid0, LineagentToVertexAndBeyondGrid1, vertice_max, gridSize):
+        for i in range(len(grid)):
+            if ((grid[i]).intersects(self._NotVisiblePolygon(vertices_coords, LineagentToVertexAndBeyondGrid0, LineagentToVertexAndBeyondGrid1, vertice_max, gridSize))):
+                occupation_grid[i] = (0.5)
+
+    #needs to be fixed so that it can adjust itself to other sizes of grid
+    def _NotVisiblePolygon(self, vertices_coords: List[Tuple[float, float]], LineagentToVertexAndBeyondGrid0, LineagentToVertexAndBeyondGrid1, vertice_max, gridSize):
+        grid_boundary = box(0, 0, gridSize[0], gridSize[1])
+        x, y = grid_boundary.exterior.xy
+        plt.plot(x, y, color="blue", alpha=0.5)
+        intersection1 = grid_boundary.intersection(LineagentToVertexAndBeyondGrid0)
+        intersection2 = grid_boundary.intersection(LineagentToVertexAndBeyondGrid1)
+
+        intersection11 = intersection1.coords[1]
+        intersection22 = intersection2.coords[1]
+
+        # Create the polygon representing the field of view behind the obsticlePolygons
+        rear_polygon_coords = [vertices_coords[0]]
+
+        #rear_polygon_coords.append(vertices_coords[0])
+        rear_polygon_coords.append(vertice_max)
+        rear_polygon_coords.append(vertices_coords[1])
+        rear_polygon_coords.append(intersection22)
+        if ((intersection1.coords[1][0] == 0.0 and
+             intersection1.coords[1][1] != gridSize[1] and
+             intersection2.coords[1][0] != 0.0 and
+             intersection2.coords[1][1] == gridSize[1]) or
+             (intersection1.coords[1][0] != 0.0 and
+             intersection2.coords[1][0] == 0.0 and
+             intersection2.coords[1][1] != gridSize[1] and
+             intersection1.coords[1][1] == gridSize[1])):
+            rear_polygon_coords.append((0.0, gridSize[1]))
+        elif ((intersection1.coords[1][0] == gridSize[0] and
+             intersection2.coords[1][0] != gridSize[0] and
+             intersection2.coords[1][1] == gridSize[1]) or
+             (intersection1.coords[1][0] != gridSize[0] and
+             intersection2.coords[1][0] == gridSize[0] and
+             intersection1.coords[1][1] == gridSize[1])):
+            rear_polygon_coords.append((gridSize[0], gridSize[1]))
+        elif ((intersection1.coords[1][0] == 0.0 and
+             intersection2.coords[1][0] != 0.0 and
+             intersection2.coords[1][0] != 0.0) or
+             (intersection1.coords[1][0] != 0.0 and
+             intersection2.coords[1][0] == 0.0 and
+             intersection2.coords[1][1] != 0.0)):
+            rear_polygon_coords.append((0.0, 0.0))
+        elif ((intersection1.coords[1][0] == gridSize[0] and
+             intersection2.coords[1][0] != gridSize[0] and
+             intersection2.coords[1][1] == 0.0) or
+             (intersection1.coords[1][0] != gridSize[0] and
+             intersection2.coords[1][0] == gridSize[0] and
+             intersection1.coords[1][1] == 0.0)):
+            rear_polygon_coords.append((gridSize[0], 0.0))
+
+        rear_polygon_coords.append(intersection11)
+        rear_polygon = Polygon(rear_polygon_coords)
+
+        x, y = rear_polygon.exterior.xy
+        plt.plot(x, y, color="black", alpha=0.5)
+        plt.fill(x ,y, alpha=1, color="blue")
+
+        return rear_polygon
+    
+    def _fillOccupancyGrid(self, grid: list, occupation_grid: List[float], obsticlePolygons):
+        for i in range(len(grid)):
+            if ((occupation_grid[i]) == 0.5):
+                occupation_grid[i] = (0.5)
+            elif (grid[i]).intersects(obsticlePolygons):
+                occupation_grid[i] = (1.0)
+            else:
+                occupation_grid[i] = (0.0)
+        
+    def _1dTo2dGrid(self, occupation_grid: List[float], rows: int, columns: int):
+        if rows * columns != len(occupation_grid):
+            return None  # Invalid input, the list size doesn't match the grid size
+    
+        twoDimensionalOccupationGrid = []
+        for i in range(rows):
+            row = occupation_grid[i * columns: (i + 1) * columns]
+            twoDimensionalOccupationGrid.append(row)
+    
+        return twoDimensionalOccupationGrid
+    
+    def _printOccupationGrid(self, occupation_grid: List[float]):
+        for row in occupation_grid:
+            print(row)
+
+        plt.title("line")
+        plt.show()
 
--- scripts/test_FOV_Perception.py
+++ scripts/test_FOV_Perception.py
@@ -2,15 +2,19 @@ exec(open("scripts/sys_path_hack.py").read())
 
 from pedgrid.lib.BaseObject import BaseObject
 
+import matplotlib.pyplot as plt
+
 from pedgrid.perception.BasePerception import BasePerception
 from pedgrid.perception.FOVPerception import FOVPerception
 import random
 from shapely.geometry import Polygon, MultiPolygon
 
+from shapely.geometry import LineString
+
 
 # Create some BaseObject instances
-object1 = BaseObject((4, 4), (5, 5), "obstacle")
-object2 = BaseObject((6, 6), (7, 7), "agent")
+object1 = BaseObject((4, 5), (5, 4), "obstacle")
+object2 = BaseObject((6, 7), (7, 6), "agent")
 # Create the FOVPerception instance
 fov_perception = FOVPerception()
 
